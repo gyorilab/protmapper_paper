6f859ec097f46c2f9f47e18618f45d88084033d8
John Bachman <bachmanjohn@gmail.com>
2018-12-19 05:20:56 -0500

    Update get_db_sites

--- get_db_sites.py
+++ get_db_sites.py
@@ -1,6 +1,8 @@
 import pickle
+import itertools
 from collections import Counter, defaultdict
 from indra.databases import uniprot_client
+from indra.tools import assemble_corpus as ac
 from indra.util import read_unicode_csv, write_unicode_csv
 from indra_db.client import get_statements_by_gene_role_type
 from sitemapper import SiteMapper
@@ -20,33 +22,41 @@ def get_db_phos_stmts(reload=False, filename='output/db_phos_stmts.pkl'):
     return phos_stmts
 
 
-def get_db_sites(reload=False, filename='output/gene_sites.csv',
-                 phos_stmts=None):
-    if reload is False:
-        return list(set([tuple(r) for r in read_unicode_csv(filename)]))
-    else:
-        if phos_stmts is None:
-            raise ValueError('phos_stmts must be provided.')
+def preprocess_db_stmts(stmts, prefix='output/db_phos_stmts'):
+    """Take the statements from the database and grounding map them; """
+    print("Mapping grounding")
+    gmap_stmts = ac.map_grounding(stmts)
+    ac.dump_statements(gmap_stmts, prefix + '_gmap.pkl')
+    print("Sorting and filtering")
+    # Next, eliminate exact duplicates
+    stmts_by_deep_hash = [(s.get_hash(shallow=False), s)
+                          for s in gmap_stmts]
+    stmts_by_deep_hash.sort(key=lambda x: x[0])
+    uniq_stmts = []
+    for k, group in itertools.groupby(stmts_by_deep_hash, key=lambda x: x[0]):
+        uniq_stmts.append(list(group)[0][1])
+    # Filter to statements with position
+    site_stmts = [s for s in uniq_stmts if s.residue and s.position]
+    ac.dump_statements(site_stmts, prefix + '_gmap_uniq_pos.pkl')
+    return site_stmts
 
-        filter_no_enzyme = [s for s in phos_stmts
-                            if s.agent_list()[0] is not None]
 
-        gene_sites = []
-        for ix, s in enumerate(filter_no_enzyme):
-            if ix % 1000 == 0:
-                print(ix)
-            up_id = s.sub.db_refs.get('UP')
-            if not up_id:
-                continue
-            up_mnemonic = uniprot_client.get_mnemonic(up_id)
-            if not s.residue or not s.position:
-                continue
-            gene_name = uniprot_client.get_gene_name(up_id)
-            gene_sites.append((up_id, gene_name, up_mnemonic, s.residue,
-                               s.position))
-
-        write_unicode_csv(filename, gene_sites)
-        return gene_sites
+def get_stmts_by_site(phos_stmts, filename):
+    # First filter statements to those that have objects with uniprot IDs
+    filt_stmts = [s for s in phos_stmts if s.sub.db_refs.get('UP')]
+    gene_sites = []
+    stmts_by_site = {}
+    for s in filt_stmts:
+        if s.enz is None:
+            continue
+        site = (s.sub.db_refs.get('UP'))
+        if site in stmts_by_site:
+            stmts_by_site[site].append(s)
+        else:
+            stmts_by_site[site] = [s]
+    with open(filename, 'wb') as f:
+        pickle.dump(stmts_by_site, f)
+    return stmts_by_site
 
 
 def site_cache_stats():
@@ -72,13 +82,18 @@ def site_cache_stats():
     return ctr, examples
 
 
-
 if __name__ == '__main__':
     """
-    phos_stmts = get_db_phos_stmts(reload=True)
-    gene_sites = get_db_sites(reload=True, phos_stmts=phos_stmts)
+    phos_stmts = get_db_phos_stmts(reload=False,
+                                   filename='output/db_phos_stmts.pkl')
+    preproc_stmts = preprocess_db_stmts(phos_stmts,
+                                        prefix='output/db_phos_stmts')
     """
-    """
+    preproc_stmts = \
+            ac.load_statements('output/db_phos_stmts_gmap_uniq_pos_enz.pkl')
+    stmts_by_site = get_stmts_by_site(preproc_stmts, 'output/stmts_by_site.pkl')
+
+    #gene_sites = get_db_sites(reload=True, phos_stmts=phos_stmts)
     gene_sites = get_db_sites(reload=False)
     import random
     random.seed(1)
@@ -90,6 +105,6 @@ if __name__ == '__main__':
     with open('sm_cache.pkl', 'wb') as f:
         import pickle
         pickle.dump(sm._cache, f)
-    """
     site_ctr, examples = site_cache_stats()
+    """
 
