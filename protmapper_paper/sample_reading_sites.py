4aa13b5b47d15d35dd0f6238c0cf9517deab51e7
Ben Gyori <ben.gyori@gmail.com>
2019-03-07 01:08:35 -0500

    Add code to sample reading sites

---
+++ protmapper_paper/sample_reading_sites.py
@@ -0,0 +1,26 @@
+import numpy
+import pickle
+import pandas
+
+df = pandas.read_csv('../output/site_info.csv')
+df = df[(df['SOURCE'] == 'reach') | (df['SOURCE'] == 'sparser')]
+df = df[df['VALID'] == False]
+freq = numpy.array(df.FREQ) / numpy.sum(df.FREQ)
+samples = [list(numpy.random.multinomial(1, freq)).index(1)
+           for _ in range(100)]
+df_sample = df.iloc[samples]
+
+with open('../output/indra_reach.sites.pkl', 'rb') as fh:
+    reach_sites = pickle.load(fh)
+with open('../output/indra_sparser.sites.pkl', 'rb') as fh:
+    sparser_sites = pickle.load(fh)
+
+sentences = []
+for _, row in df_sample.iterrows():
+    std = reach_sites if row['SOURCE'] == 'reach' else sparser_sites
+    key = (row['UP_ID'], row['ORIG_RES'], str(int(row['ORIG_POS'])))
+    stmts = std[key]['rhs']
+    sentences.append(stmts[0].evidence[0].text)
+
+df_sample['EVIDENCE'] = sentences
+df_sample.to_excel('reading_sites_sample.xlsx')
